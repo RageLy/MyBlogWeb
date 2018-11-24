daa8272422e5aecca08e312f5ef3de1ccd9a2234
huahuayufeng <heming_wei@126.com>
2018-11-24 13:00:17 +0800

    添加播放器和地图定位

--- MyBlogWeb/urls.py
+++ MyBlogWeb/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('MyBlog.urls')),
     path("favicon.ico", RedirectView.as_view(url='static/favicon.ico')),
+
+
+    # path('', include('MyBlogAdmin.urls')),
+
     path('adminht/', admin.site.urls),
+
+    # path('markdown/',include('django_markdown.urls')),
+
+
+
+
+
 ]
 
